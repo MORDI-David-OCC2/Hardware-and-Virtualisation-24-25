b74f3682c12e9ef325dca9ee85548e4248f63c30
inessk9 <ines.kaci@edu.devinci.fr>
2024-11-08 15:40:18 +0100

    Add USART prototype

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+
+
+#![no_std]
+#![no_main]
+
+/*
+/*use core::panic::PanicInfo;*/
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+use panic_halt as _;
+mod gpio; //Import du module gpio ( fichier gpio.rs)
+*/
+
+//Ajout test 7/11/24
+
+
+extern crate panic_halt; // Gestionnaire de panique (peut être différent selon votre projet)
+mod usart;
+
+
+#[no_mangle]
+fn main() {
+    usart::init_uart_reg(); // Initialisation de l'USART
+
+    // Envoi d'un message
+    usart::send_message("Hello, USART!");
+    
+    loop {
+        // Boucle infinie pour garder le programme en cours d'exécution
+        //partie ajouté le 08/11/24 pour receptionner l'octet et le réemettre 
+        let received_byte = usart::receive_byte();
+        // Envoi d'un message indiquant que l'octet a été reçu
+        usart::send_message("Recu : ");
+        usart::transmit_byte(received_byte);
+    }
+}
+
+
+//fin ajout test 7/11/24
+
+
+
+/*#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
+}*/
+/*
+#[entry]
+fn main() -> ! {
+    unsafe {
+        let _ = hprintln!("Hello, world!").unwrap();
+        //gpio::set_pin_output(5); //Utilisation de la fonction du module gpio
+        //gpio::set_pin_high(5); //Allume la broche 5 (exemple)
+        debug::exit(debug::EXIT_SUCCESS);
+        loop{}
+    }
+        
+    
+}*/
+
+
+
+
---
+++ src/usart.rs
@@ -0,0 +1,113 @@
+#![no_std]
+use core::ptr::{read_volatile, write_volatile};
+pub fn init_uart_reg()
+
+{
+
+    let ubrr=(FOSC/(16*BAUD))-1;
+
+ 
+
+    // Setup USART Baud Rate registers value
+
+    // Baud prescaler register is split on two bytes
+
+    let ubrr0h: u8 = (ubrr >> 8) as u8;
+
+    let ubrr0l: u8 = (ubrr) as u8;
+
+ 
+
+    // Enable receiver and transmitter
+
+    let ucsr0b: u8 = (1<<RXCEN0) | (1<<TXEN0);
+
+    // Set frame format: 8data, 1stop bit
+
+    let ucsr0c: u8 = (1<<USBS0) | (3<<UCSZ00);
+
+ 
+
+    unsafe {
+
+        write_volatile(ADD_UBRR0H, ubrr0h);
+
+        write_volatile(ADD_UBRR0L, ubrr0l);
+
+        write_volatile(ADD_UCSR0C, ucsr0c);
+
+        write_volatile(ADD_UCSR0B, ucsr0b);
+
+    }
+
+}
+
+pub const FOSC : u32 = 1843200;  // Clock Speed
+
+pub const BAUD : u32 = 9600; // Baud Rate
+
+ 
+
+pub const UCSZ00 : u8 = 1;
+
+pub const RXCEN0 : u8 = 4;
+
+pub const TXEN0  : u8 = 3;
+
+pub const USBS0  : u8 = 3;
+
+ 
+
+pub const ADD_UCSR0C: *mut u8 = 0xC2 as *mut u8;
+
+pub const ADD_UCSR0B: *mut u8 = 0xC1 as *mut u8;
+
+pub const ADD_UBRR0H: *mut u8 = 0xC5 as *mut u8;//usart br reg high
+
+pub const ADD_UBRR0L: *mut u8 = 0xC4 as *mut u8;//usart br reg low
+
+
+pub const ADD_UCSR0A: *mut u8 = 0xC0 as *mut u8;
+
+pub const UDRE0  : u8 = 5;
+
+pub const ADD_UDR0  : *mut u8 = 0xC6 as *mut u8;
+
+pub fn transmit_byte(byte : u8)
+
+{
+    unsafe
+    {
+        //let ucsr0a = ptr::read_volatile(ADD_UCSR0A);
+        //let udre0 = ucsr0a & (1 << UDRE0) ;
+
+        // Wait for empty transmit buffer
+        while read_volatile(ADD_UCSR0A) & (1<<UDRE0)==0{}
+
+        write_volatile(ADD_UDR0,byte);
+
+    }
+
+}
+
+pub fn send_message(s:&str){
+
+    s.as_bytes()
+
+    .iter()
+
+    .for_each(|b| transmit_byte(*b));
+
+}
+//fonction ajouté le 08/11/24 pour tester la reception de message 
+
+pub fn receive_byte() -> u8 {
+    unsafe {
+        // Attendre que des données soient disponibles dans le tampon de réception
+        while read_volatile(ADD_UCSR0A) & (1 << 7) == 0 {} // Le bit 7 (RXC0) de UCSR0A indique la disponibilité des données
+
+        // Lire les données du registre de réception
+        read_volatile(ADD_UDR0)
+    }
+}
+
